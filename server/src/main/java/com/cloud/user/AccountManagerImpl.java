ba0c34cbbc16724f3fb1bb20b14ae33630d60888
Damian Guppy <damian@Damians-Air.corp.lumity.com.au>
2026-05-19 01:10:02 +0800

    Phase 4: Extract AccountAccessService from AccountManagerImpl (slice account10)

--- server/src/main/java/com/cloud/user/AccountManagerImpl.java
+++ server/src/main/java/com/cloud/user/AccountManagerImpl.java
@@ -59,7 +59,6 @@ import org.apache.cloudstack.acl.apikeypair.ApiKeyPairPermission;
 import org.apache.cloudstack.acl.apikeypair.ApiKeyPairService;
 import org.apache.cloudstack.acl.dao.ApiKeyPairDao;
 import org.apache.cloudstack.acl.dao.ApiKeyPairPermissionsDao;
-import org.apache.cloudstack.affinity.AffinityGroup;
 import org.apache.cloudstack.affinity.dao.AffinityGroupDao;
 import org.apache.cloudstack.api.APICommand;
 import org.apache.cloudstack.api.ApiConstants;
@@ -103,7 +102,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 
-import com.cloud.api.ApiDBUtils;
 import com.cloud.api.auth.SetupUserTwoFactorAuthenticationCmd;
 import com.cloud.api.query.vo.ControlledViewEntity;
 import com.cloud.configuration.Config;
@@ -147,7 +145,6 @@ import com.cloud.network.dao.RemoteAccessVpnDao;
 import com.cloud.network.dao.RemoteAccessVpnVO;
 import com.cloud.network.dao.SslCertDao;
 import com.cloud.network.dao.VpnUserDao;
-import com.cloud.network.router.VirtualRouter;
 import com.cloud.network.security.SecurityGroupManager;
 import com.cloud.network.security.SecurityGroupService;
 import com.cloud.network.security.SecurityGroupVO;
@@ -174,7 +171,6 @@ import com.cloud.storage.dao.VMTemplateDao;
 import com.cloud.storage.dao.VolumeDao;
 import com.cloud.storage.snapshot.SnapshotManager;
 import com.cloud.template.TemplateManager;
-import com.cloud.template.VirtualMachineTemplate;
 import com.cloud.user.Account.State;
 import com.cloud.user.dao.UserDataDao;
 import com.cloud.utils.NumbersUtil;
@@ -236,6 +232,8 @@ public class AccountManagerImpl extends ManagerBase implements AccountManager, M
     protected UserAuthenticationService userAuthenticationService;
     @Inject
     protected AccountStateService accountStateService;
+    @Inject
+    protected AccountAccessService accountAccessService;
     @Inject
     private ConfigurationDao _configDao;
     @Inject
@@ -621,202 +619,46 @@ public class AccountManagerImpl extends ManagerBase implements AccountManager, M
 
     @Override
     public boolean isAdmin(Long accountId) {
-        if (accountId != null) {
-            AccountVO acct = _accountDao.findById(accountId);
-            if (acct == null) {
-                return false;  //account is deleted or does not exist
-            }
-            return (isRootAdmin(accountId)) || (isDomainAdmin(accountId)) || (isResourceDomainAdmin(accountId)) || (acct.getType() == Account.Type.READ_ONLY_ADMIN);
-
-        }
-        return false;
+        return accountAccessService.isAdmin(accountId);
     }
 
     @Override
     public boolean isRootAdmin(Long accountId) {
-        if (accountId != null) {
-            AccountVO acct = _accountDao.findById(accountId);
-            if (acct == null) {
-                return false;  //account is deleted or does not exist
-            }
-            for (SecurityChecker checker : _securityCheckers) {
-                try {
-                    if (checker.checkAccess(acct, null, null, "SystemCapability")) {
-                        if (logger.isTraceEnabled()) {
-                            logger.trace("Root Access granted to " + acct + " by " + checker.getName());
-                        }
-                        return true;
-                    }
-                } catch (PermissionDeniedException ex) {
-                    return false;
-                }
-            }
-        }
-        return false;
+        return accountAccessService.isRootAdmin(accountId);
     }
 
     @Override
     public boolean isDomainAdmin(Long accountId) {
-        if (accountId != null) {
-            AccountVO acct = _accountDao.findById(accountId);
-            if (acct == null) {
-                return false;  //account is deleted or does not exist
-            }
-            for (SecurityChecker checker : _securityCheckers) {
-                try {
-                    if (checker.checkAccess(acct, null, null, "DomainCapability")) {
-                        if (logger.isTraceEnabled()) {
-                            logger.trace("DomainAdmin Access granted to " + acct + " by " + checker.getName());
-                        }
-                        return true;
-                    }
-                } catch (PermissionDeniedException ex) {
-                    return false;
-                }
-            }
-        }
-        return false;
+        return accountAccessService.isDomainAdmin(accountId);
     }
 
     @Override
     public boolean isNormalUser(long accountId) {
-        AccountVO acct = _accountDao.findById(accountId);
-        return acct != null && acct.getType() == Account.Type.NORMAL;
+        return accountAccessService.isNormalUser(accountId);
     }
 
     @Override
     public boolean isResourceDomainAdmin(Long accountId) {
-        if (accountId != null) {
-            AccountVO acct = _accountDao.findById(accountId);
-            if (acct == null) {
-                return false;  //account is deleted or does not exist
-            }
-            for (SecurityChecker checker : _securityCheckers) {
-                try {
-                    if (checker.checkAccess(acct, null, null, "DomainResourceCapability")) {
-                        if (logger.isTraceEnabled()) {
-                            logger.trace("ResourceDomainAdmin Access granted to " + acct + " by " + checker.getName());
-                        }
-                        return true;
-                    }
-                } catch (PermissionDeniedException ex) {
-                    return false;
-                }
-            }
-        }
-        return false;
+        return accountAccessService.isResourceDomainAdmin(accountId);
     }
 
     public boolean isInternalAccount(long accountId) {
-        Account account = _accountDao.findById(accountId);
-        if (account == null) {
-            return false;  //account is deleted or does not exist
-        }
-        return isRootAdmin(accountId) || (account.getType() == Account.Type.ADMIN);
+        return accountAccessService.isInternalAccount(accountId);
     }
 
     @Override
     public void checkAccess(Account caller, Domain domain) throws PermissionDeniedException {
-        for (SecurityChecker checker : _securityCheckers) {
-            if (checker.checkAccess(caller, domain)) {
-                if (logger.isDebugEnabled()) {
-                    logger.debug("Access granted to " + caller + " to " + domain + " by " + checker.getName());
-                }
-                return;
-            }
-        }
-        throw new PermissionDeniedException("There's no way to confirm " + caller + " has access to " + domain);
+        accountAccessService.checkAccess(caller, domain);
     }
 
     @Override
     public void checkAccess(Account caller, AccessType accessType, boolean sameOwner, ControlledEntity... entities) {
-        checkAccess(caller, accessType, sameOwner, null, entities);
+        accountAccessService.checkAccess(caller, accessType, sameOwner, entities);
     }
 
     @Override
     public void checkAccess(Account caller, AccessType accessType, boolean sameOwner, String apiName, ControlledEntity... entities) {
-
-        //check for the same owner
-        Long ownerId = null;
-        ControlledEntity prevEntity = null;
-        if (sameOwner) {
-            for (ControlledEntity entity : entities) {
-                if (ownerId == null) {
-                    ownerId = entity.getAccountId();
-                } else if (! ownerId.equals(entity.getAccountId())) {
-                    throw new PermissionDeniedException("Entity " + entity + " and entity " + prevEntity + " belong to different accounts");
-                }
-                prevEntity = entity;
-            }
-        }
-
-        if (caller.getId() == Account.ACCOUNT_ID_SYSTEM || isRootAdmin(caller.getId())) {
-            // no need to make permission checks if the system/root admin makes the call
-            if (logger.isTraceEnabled()) {
-                logger.trace("No need to make permission check for System/RootAdmin account, returning true");
-            }
-
-            return;
-        }
-
-        HashMap<Long, List<ControlledEntity>> domains = new HashMap<>();
-
-        for (ControlledEntity entity : entities) {
-            long domainId = entity.getDomainId();
-            if (entity.getAccountId() != -1 && domainId == -1) { // If account exists domainId should too so calculate
-                // it. This condition might be hit for templates or entities which miss domainId in their tables
-                Account account = ApiDBUtils.findAccountById(entity.getAccountId());
-                domainId = account != null ? account.getDomainId() : -1;
-            }
-            if (entity.getAccountId() != -1 && domainId != -1 && !(entity instanceof VirtualMachineTemplate)
-                    && !(entity instanceof Network && (accessType == AccessType.UseEntry || accessType == AccessType.OperateEntry))
-                    && !(entity instanceof AffinityGroup) && !(entity instanceof VirtualRouter)) {
-                List<ControlledEntity> toBeChecked = domains.get(entity.getDomainId());
-                // for templates, we don't have to do cross domains check
-                if (toBeChecked == null) {
-                    toBeChecked = new ArrayList<>();
-                    domains.put(domainId, toBeChecked);
-                }
-                toBeChecked.add(entity);
-            }
-            boolean granted = false;
-            for (SecurityChecker checker : _securityCheckers) {
-                if (checker.checkAccess(caller, entity, accessType, apiName)) {
-                    if (logger.isDebugEnabled()) {
-                        User user = CallContext.current().getCallingUser();
-                        String userName = "";
-                        if (user != null)
-                            userName = user.getUsername();
-                        logger.debug("Access to {} granted to {} by {} on behalf of user {}", entity, caller, checker.getName(), userName);
-                    }
-                    granted = true;
-                    break;
-                }
-            }
-
-            if (!granted) {
-                assert false : "How can all of the security checkers pass on checking this check: " + entity;
-            throw new PermissionDeniedException("There's no way to confirm " + caller + " has access to " + entity);
-            }
-        }
-
-        for (Map.Entry<Long, List<ControlledEntity>> domain : domains.entrySet()) {
-            for (SecurityChecker checker : _securityCheckers) {
-                Domain d = _domainMgr.getDomain(domain.getKey());
-                if (d == null || d.getRemoved() != null) {
-                    throw new PermissionDeniedException("Domain is not found.", caller, domain.getValue());
-                }
-                try {
-                    checker.checkAccess(caller, d);
-                } catch (PermissionDeniedException e) {
-                    e.addDetails(caller, domain.getValue());
-                    throw e;
-                }
-            }
-        }
-
-        // check that resources belong to the same account
-
+        accountAccessService.checkAccess(caller, accessType, sameOwner, apiName, entities);
     }
 
     @Override
@@ -834,30 +676,7 @@ public class AccountManagerImpl extends ManagerBase implements AccountManager, M
 
     @Override
     public Long checkAccessAndSpecifyAuthority(Account caller, Long zoneId) {
-        // We just care for resource domain admins for now, and they should be permitted to see only their zone.
-        if (isResourceDomainAdmin(caller.getAccountId())) {
-            if (zoneId == null) {
-                return getZoneIdForAccount(caller);
-            } else if (zoneId.compareTo(getZoneIdForAccount(caller)) != 0) {
-                throw new PermissionDeniedException("Caller " + caller + "is not allowed to access the zone " + zoneId);
-            } else {
-                return zoneId;
-            }
-        } else {
-            return zoneId;
-        }
-    }
-
-    private Long getZoneIdForAccount(Account account) {
-
-        // Currently just for resource domain admin
-        List<DataCenterVO> dcList = _dcDao.findZonesByDomainId(account.getDomainId());
-        if (CollectionUtils.isNotEmpty(dcList)) {
-            return dcList.get(0).getId();
-        } else {
-            throw new CloudRuntimeException("Failed to find any private zone for Resource domain admin.");
-        }
-
+        return accountAccessService.checkAccessAndSpecifyAuthority(caller, zoneId);
     }
 
     @DB
@@ -2910,90 +2729,32 @@ public class AccountManagerImpl extends ManagerBase implements AccountManager, M
 
     @Override
     public void checkAccess(Account account, ServiceOffering so, DataCenter zone) throws PermissionDeniedException {
-        for (SecurityChecker checker : _securityCheckers) {
-            if (checker.checkAccess(account, so, zone)) {
-                if (logger.isDebugEnabled()) {
-                    logger.debug("Access granted to " + account + " to " + so + " by " + checker.getName());
-                }
-                return;
-            }
-        }
-
-        assert false : "How can all of the security checkers pass on checking this caller?";
-        throw new PermissionDeniedException("There's no way to confirm " + account + " has access to " + so);
+        accountAccessService.checkAccess(account, so, zone);
     }
 
     @Override
     public void checkAccess(Account account, DiskOffering dof, DataCenter zone) throws PermissionDeniedException {
-        for (SecurityChecker checker : _securityCheckers) {
-            if (checker.checkAccess(account, dof, zone)) {
-                if (logger.isDebugEnabled()) {
-                    logger.debug("Access granted to " + account + " to " + dof + " by " + checker.getName());
-                }
-                return;
-            }
-        }
-
-        assert false : "How can all of the security checkers pass on checking this caller?";
-        throw new PermissionDeniedException("There's no way to confirm " + account + " has access to " + dof);
+        accountAccessService.checkAccess(account, dof, zone);
     }
 
     @Override
     public void checkAccess(Account account, NetworkOffering nof, DataCenter zone) throws PermissionDeniedException {
-        for (SecurityChecker checker : _securityCheckers) {
-            if (checker.checkAccess(account, nof, zone)) {
-                if (logger.isDebugEnabled()) {
-                    logger.debug("Access granted to " + account + " to " + nof + " by " + checker.getName());
-                }
-                return;
-            }
-        }
-
-        assert false : "How can all of the security checkers pass on checking this caller?";
-        throw new PermissionDeniedException("There's no way to confirm " + account + " has access to " + nof);
+        accountAccessService.checkAccess(account, nof, zone);
     }
 
     @Override
     public void checkAccess(Account account, VpcOffering vof, DataCenter zone) throws PermissionDeniedException {
-        for (SecurityChecker checker : _securityCheckers) {
-            if (checker.checkAccess(account, vof, zone)) {
-                if (logger.isDebugEnabled()) {
-                    logger.debug("Access granted to " + account + " to " + vof + " by " + checker.getName());
-                }
-                return;
-            }
-        }
-
-        assert false : "How can all of the security checkers pass on checking this caller?";
-        throw new PermissionDeniedException("There's no way to confirm " + account + " has access to " + vof);
+        accountAccessService.checkAccess(account, vof, zone);
     }
 
     @Override
     public void checkAccess(Account account, BackupOffering bof) throws PermissionDeniedException {
-        for (SecurityChecker checker : _securityCheckers) {
-            if (checker.checkAccess(account, bof)) {
-                if (logger.isDebugEnabled()) {
-                    logger.debug("Access granted to " + account + " to " + bof + " by " + checker.getName());
-                }
-                return;
-            }
-        }
-
-        assert false : "How can all of the security checkers pass on checking this caller?";
-        throw new PermissionDeniedException("There's no way to confirm " + account + " has access to " + bof);
+        accountAccessService.checkAccess(account, bof);
     }
 
     @Override
     public void checkAccess(User user, ControlledEntity entity) throws PermissionDeniedException {
-        for (SecurityChecker checker : _securityCheckers) {
-            if (checker.checkAccess(user, entity)) {
-                if (logger.isDebugEnabled()) {
-                    logger.debug("Access granted to " + user + "to " + entity + "by " + checker.getName());
-                }
-                return;
-            }
-        }
-        throw new PermissionDeniedException("There's no way to confirm " + user + " has access to " + entity);
+        accountAccessService.checkAccess(user, entity);
     }
 
     @Override
