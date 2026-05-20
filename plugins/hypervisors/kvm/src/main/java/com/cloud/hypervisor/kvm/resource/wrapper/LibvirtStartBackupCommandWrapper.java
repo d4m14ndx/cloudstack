e31cff1feab764df384412254eabfe05108a6b80
Damian Guppy <damian@Damians-Air.corp.lumity.com.au>
2026-05-21 00:04:30 +0800

    Add KVM backup checkpoint command wrappers

---
+++ plugins/hypervisors/kvm/src/main/java/com/cloud/hypervisor/kvm/resource/wrapper/LibvirtStartBackupCommandWrapper.java
@@ -0,0 +1,191 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package com.cloud.hypervisor.kvm.resource.wrapper;
+
+import com.cloud.agent.api.Answer;
+import com.cloud.hypervisor.kvm.resource.LibvirtComputingResource;
+import com.cloud.resource.CommandWrapper;
+import com.cloud.resource.ResourceWrapper;
+import com.cloud.utils.exception.CloudRuntimeException;
+import com.cloud.utils.script.Script;
+import org.apache.cloudstack.backup.StartBackupAnswer;
+import org.apache.cloudstack.backup.StartBackupCommand;
+import org.apache.cloudstack.utils.cryptsetup.KeyFile;
+import org.apache.cloudstack.utils.qemu.QemuImageOptions;
+import org.apache.cloudstack.utils.qemu.QemuImg;
+import org.apache.cloudstack.utils.qemu.QemuImgException;
+import org.apache.cloudstack.utils.qemu.QemuImgFile;
+import org.apache.cloudstack.utils.qemu.QemuObject;
+import org.apache.cloudstack.utils.security.ParserUtils;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.libvirt.LibvirtException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+
+@ResourceWrapper(handles = StartBackupCommand.class)
+public class LibvirtStartBackupCommandWrapper extends CommandWrapper<StartBackupCommand, Answer, LibvirtComputingResource> {
+
+    private static final String VIRSH_COMMAND = "virsh";
+    private static final String QEMU_IMG_SECRET_NAME = "sec0";
+
+    @Override
+    public Answer execute(StartBackupCommand command, LibvirtComputingResource resource) {
+        try {
+            validate(command.getToCheckpointId(), command.getDiskPathUuidMap());
+            if (command.isStoppedVM()) {
+                addBitmapsForStoppedVm(command, resource);
+            } else {
+                createCheckpointForRunningVm(command, resource);
+            }
+            return new StartBackupAnswer(command, true, null, System.currentTimeMillis());
+        } catch (Exception e) {
+            logger.error("Failed to start backup checkpoint [{}] on VM [{}].", command.getToCheckpointId(), command.getVmName(), e);
+            return new StartBackupAnswer(command, false, e.getMessage());
+        } finally {
+            clearPassphrases(command.getDiskPathPassphraseMap());
+        }
+    }
+
+    protected void createCheckpointForRunningVm(StartBackupCommand command, LibvirtComputingResource resource) throws Exception {
+        Map<String, String> diskPathLabelMap = resource.getDiskPathLabelMap(command.getVmName());
+        String checkpointXml = buildCheckpointXml(command, diskPathLabelMap);
+        Path checkpointXmlPath = Files.createTempFile("cloudstack-checkpoint-", ".xml");
+        try {
+            Files.write(checkpointXmlPath, checkpointXml.getBytes(StandardCharsets.UTF_8));
+            Script script = new Script(VIRSH_COMMAND, resource.getCmdsTimeout(), logger);
+            script.add("checkpoint-create");
+            script.add("--domain");
+            script.add(command.getVmName());
+            script.add("--xmlfile");
+            script.add(checkpointXmlPath.toString());
+            String result = script.execute();
+            if (result != null) {
+                throw new CloudRuntimeException(result);
+            }
+        } finally {
+            Files.deleteIfExists(checkpointXmlPath);
+        }
+    }
+
+    protected String buildCheckpointXml(StartBackupCommand command, Map<String, String> diskPathLabelMap) throws Exception {
+        DocumentBuilder docBuilder = ParserUtils.getSaferDocumentBuilderFactory().newDocumentBuilder();
+        Document document = docBuilder.newDocument();
+        Element root = document.createElement("domaincheckpoint");
+        document.appendChild(root);
+
+        appendTextElement(document, root, "name", command.getToCheckpointId());
+        if (StringUtils.isNotBlank(command.getFromCheckpointId())) {
+            Element parent = document.createElement("parent");
+            appendTextElement(document, parent, "name", command.getFromCheckpointId());
+            root.appendChild(parent);
+        }
+
+        Element disks = document.createElement("disks");
+        root.appendChild(disks);
+        for (String diskPath : command.getDiskPathUuidMap().keySet()) {
+            String diskLabel = diskPathLabelMap.get(diskPath);
+            if (StringUtils.isBlank(diskLabel)) {
+                throw new CloudRuntimeException(String.format("Unable to map disk path [%s] to a VM disk label.", diskPath));
+            }
+            Element disk = document.createElement("disk");
+            disk.setAttribute("name", diskLabel);
+            disk.setAttribute("checkpoint", "bitmap");
+            disks.appendChild(disk);
+        }
+
+        Transformer transformer = TransformerFactory.newInstance().newTransformer();
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        StringWriter writer = new StringWriter();
+        transformer.transform(new DOMSource(document), new StreamResult(writer));
+        return writer.toString();
+    }
+
+    protected void addBitmapsForStoppedVm(StartBackupCommand command, LibvirtComputingResource resource) throws IOException, QemuImgException, LibvirtException {
+        QemuImg qemuImg = new QemuImg(resource.getCmdsTimeout());
+        for (String diskPath : command.getDiskPathUuidMap().keySet()) {
+            runBitmapOperation(qemuImg, QemuImg.BitmapOperation.Add, diskPath, command.getToCheckpointId(), getPassphrase(command.getDiskPathPassphraseMap(), diskPath));
+        }
+    }
+
+    protected void runBitmapOperation(QemuImg qemuImg, QemuImg.BitmapOperation operation, String diskPath, String bitmapName, byte[] passphrase)
+            throws IOException, QemuImgException {
+        if (ArrayUtils.isEmpty(passphrase)) {
+            QemuImgFile volume = new QemuImgFile(diskPath, QemuImg.PhysicalDiskFormat.QCOW2);
+            qemuImg.bitmap(operation, volume, bitmapName);
+            return;
+        }
+
+        try (KeyFile keyFile = new KeyFile(passphrase)) {
+            QemuImageOptions imageOptions = new QemuImageOptions(QemuImg.PhysicalDiskFormat.QCOW2, diskPath, QEMU_IMG_SECRET_NAME);
+            QemuObject secret = QemuObject.prepareSecretForQemuImg(QemuImg.PhysicalDiskFormat.QCOW2, QemuObject.EncryptFormat.LUKS,
+                    keyFile.toString(), QEMU_IMG_SECRET_NAME, null);
+            qemuImg.bitmap(operation, imageOptions, Collections.singletonList(secret), bitmapName);
+        }
+    }
+
+    private void appendTextElement(Document document, Element parent, String name, String value) {
+        Element element = document.createElement(name);
+        element.setTextContent(value);
+        parent.appendChild(element);
+    }
+
+    private void validate(String checkpointId, Map<String, String> diskPathUuidMap) {
+        if (StringUtils.isBlank(checkpointId)) {
+            throw new CloudRuntimeException("Checkpoint ID is required.");
+        }
+        if (MapUtils.isEmpty(diskPathUuidMap)) {
+            throw new CloudRuntimeException("At least one disk path is required.");
+        }
+    }
+
+    private byte[] getPassphrase(Map<String, byte[]> diskPathPassphraseMap, String diskPath) {
+        if (MapUtils.isEmpty(diskPathPassphraseMap)) {
+            return null;
+        }
+        return diskPathPassphraseMap.get(diskPath);
+    }
+
+    private void clearPassphrases(Map<String, byte[]> diskPathPassphraseMap) {
+        if (MapUtils.isEmpty(diskPathPassphraseMap)) {
+            return;
+        }
+        for (byte[] passphrase : diskPathPassphraseMap.values()) {
+            if (passphrase != null) {
+                Arrays.fill(passphrase, (byte) 0);
+            }
+        }
+    }
+}
---
+++ plugins/hypervisors/kvm/src/test/java/com/cloud/hypervisor/kvm/resource/wrapper/LibvirtStartBackupCommandWrapperTest.java
@@ -0,0 +1,127 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package com.cloud.hypervisor.kvm.resource.wrapper;
+
+import com.cloud.agent.api.Answer;
+import com.cloud.hypervisor.kvm.resource.LibvirtComputingResource;
+import com.cloud.utils.script.Script;
+import org.apache.cloudstack.backup.StartBackupAnswer;
+import org.apache.cloudstack.backup.StartBackupCommand;
+import org.apache.cloudstack.utils.qemu.QemuImageOptions;
+import org.apache.cloudstack.utils.qemu.QemuImg;
+import org.apache.cloudstack.utils.qemu.QemuImgFile;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockedConstruction;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LibvirtStartBackupCommandWrapperTest {
+
+    private LibvirtStartBackupCommandWrapper wrapper;
+    private LibvirtComputingResource resource;
+
+    @Before
+    public void setUp() {
+        wrapper = new LibvirtStartBackupCommandWrapper();
+        resource = Mockito.mock(LibvirtComputingResource.class);
+        when(resource.getCmdsTimeout()).thenReturn(30);
+    }
+
+    @Test
+    public void executeCreatesRunningVmCheckpointWithDiskLabels() {
+        Map<String, String> diskPathUuidMap = new HashMap<>();
+        diskPathUuidMap.put("/var/lib/libvirt/images/root.qcow2", "root-volume");
+        StartBackupCommand command = new StartBackupCommand("i-2-VM", "checkpoint-2", "checkpoint-1", 100L,
+                "/run/cloudstack/backup.sock", diskPathUuidMap, null, false);
+        when(resource.getDiskPathLabelMap("i-2-VM")).thenReturn(Collections.singletonMap("/var/lib/libvirt/images/root.qcow2", "vda"));
+
+        try (MockedConstruction<Script> scripts = Mockito.mockConstruction(Script.class, (mock, context) -> when(mock.execute()).thenReturn(null))) {
+            Answer answer = wrapper.execute(command, resource);
+
+            Assert.assertTrue(answer instanceof StartBackupAnswer);
+            Assert.assertTrue(answer.getResult());
+            Assert.assertNotNull(((StartBackupAnswer) answer).getCheckpointCreateTime());
+            Script virsh = scripts.constructed().get(0);
+            verify(virsh).add("checkpoint-create");
+            verify(virsh).add("--domain");
+            verify(virsh).add("i-2-VM");
+            verify(virsh).add("--xmlfile");
+            verify(virsh, times(5)).add(any(String.class));
+        }
+    }
+
+    @Test
+    public void executeAddsStoppedVmBitmapToEachDisk() throws Exception {
+        Map<String, String> diskPathUuidMap = new HashMap<>();
+        diskPathUuidMap.put("/var/lib/libvirt/images/root.qcow2", "root-volume");
+        StartBackupCommand command = new StartBackupCommand("i-2-VM", "checkpoint-2", null, null,
+                null, diskPathUuidMap, null, true);
+
+        try (MockedConstruction<QemuImg> qemuImgs = Mockito.mockConstruction(QemuImg.class)) {
+            Answer answer = wrapper.execute(command, resource);
+
+            Assert.assertTrue(answer.getResult());
+            verify(qemuImgs.constructed().get(0)).bitmap(eq(QemuImg.BitmapOperation.Add), any(QemuImgFile.class), eq("checkpoint-2"));
+        }
+    }
+
+    @Test
+    public void executeAddsEncryptedStoppedVmBitmapUsingImageOptionsAndSecret() throws Exception {
+        Map<String, String> diskPathUuidMap = new HashMap<>();
+        diskPathUuidMap.put("/var/lib/libvirt/images/root.qcow2", "root-volume");
+        Map<String, byte[]> passphrases = new HashMap<>();
+        passphrases.put("/var/lib/libvirt/images/root.qcow2", "secret".getBytes());
+        StartBackupCommand command = new StartBackupCommand("i-2-VM", "checkpoint-2", null, null,
+                null, diskPathUuidMap, passphrases, true);
+
+        try (MockedConstruction<QemuImg> qemuImgs = Mockito.mockConstruction(QemuImg.class)) {
+            Answer answer = wrapper.execute(command, resource);
+
+            Assert.assertTrue(answer.getResult());
+            verify(qemuImgs.constructed().get(0)).bitmap(eq(QemuImg.BitmapOperation.Add), any(QemuImageOptions.class), any(), eq("checkpoint-2"));
+        }
+    }
+
+    @Test
+    public void executeFailsWhenRunningVmDiskPathCannotBeMappedToDiskLabel() {
+        Map<String, String> diskPathUuidMap = new HashMap<>();
+        diskPathUuidMap.put("/var/lib/libvirt/images/root.qcow2", "root-volume");
+        StartBackupCommand command = new StartBackupCommand("i-2-VM", "checkpoint-2", null, null,
+                null, diskPathUuidMap, null, false);
+        when(resource.getDiskPathLabelMap("i-2-VM")).thenReturn(Collections.emptyMap());
+
+        Answer answer = wrapper.execute(command, resource);
+
+        Assert.assertFalse(answer.getResult());
+        Assert.assertTrue(answer.getDetails().contains("Unable to map disk path"));
+    }
+}
